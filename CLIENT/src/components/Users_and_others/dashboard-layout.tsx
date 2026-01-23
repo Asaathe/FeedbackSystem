8c85b04ef2b082537876a62956f41edf6b3c1a3f
asathe <geraldmelicio.it@gmail.com>
2026-01-23 22:15:45 +0800

    remove notif icon

--- CLIENT/src/components/Users_and_others/dashboard-layout.tsx
+++ CLIENT/src/components/Users_and_others/dashboard-layout.tsx
@@ -161,7 +161,6 @@ export function DashboardLayout({ children, currentPage, onNavigate, onLogout, r
             >
               {sidebarOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
             </Button>
-            <h2 className="text-xl capitalize">{currentPage.replace('-', ' ')}</h2>
           </div>
           <div className="flex items-center gap-2">
             {/* User Profile Dropdown */}
